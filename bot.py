aca03b23f97ca3abe322c1b57af74b48375750ea
Kaneki <cool5tarXV@gmail.com>
2021-07-20 18:57:11 +0800

    better sourcing

--- bot.py
+++ bot.py
@@ -7,41 +7,123 @@ import datetime
 import json
 import aiohttp
 import urllib.parse
-import funcs
 import asyncio
-from PIL import Image
+from os import listdir
 import random
 from discord_components import DiscordComponents
 from paginate import paginate
 
-
-devs = []
-
+devs = [746904488396324864]
+"""
 def getmodule(repolink):
     repostuffs = repolink.replace("https://github.com/","")
+    print(repostuffs)
     repostuffs = repostuffs.replace("/"," ")
-    repolist = repostuffs.strip
+    print(repostuffs)
+    repolist = repostuffs.split()
+    print(repolist)
     author = repolist[0]
     modname = repolist[1]
     linktocfg = f"https://raw.githubusercontent.com/{author}/{modname}/main/cogcfg.json"
     return linktocfg
+"""
 
-async def instmod(ctx,cfg):
+
+async def addrepo(ctx,alias,link):
+    try:
+        release = f"{link}/release.json"
+        release = release.replace("//","/")
+        release = release.replace("https:/","https://")
+        packages = f"{link}/packages.json"
+        packages = packages.replace("//","/")
+        packages = packages.replace("https:/","https://")
+        rel = requests.request("GET",url=release)
+        rel = rel.json()
+        pkgs = requests.request("GET",url=packages)
+        pkgs = pkgs.json()
+    except:
+        await ctx.send("ERROR")
+        return await ctx.author.send("ERROR: 'Unable to get release.json/packages.json from source'")
+    try:
+        with open("sources.json","r") as k:
+            srcs = json.load(k)
+    except:
+        await ctx.send("ERROR")
+        return await ctx.author.send("ERROR: 'Not configured `sources.json` yet. Please run [p]csetup'")
+    for item in srcs:
+        if item == alias:
+            await ctx.send("ERROR")
+            return await ctx.author.send("ERROR:'Alias for source exists, please pick new alias'")
+    for item in srcs:
+        if srcs[item] == link:
+            await ctx.send("ERROR")
+            return await ctx.author.send("ERROR:'Source exists, please use edit action'")
+    srcs[alias] = link
+    with open("sources.json","w") as k:
+        json.dump(srcs,k)
+    embed = discord.Embed(title="Successfully Added Repo",description=f"Successfully added {alias} to sources.",color=discord.Color.green())
+    await ctx.send(embed=embed)
+
+async def sourcelist(ctx):
+    paged = False
+    pages = []
+    embed = discord.Embed(title="Repos",description=f"Sources:",color=discord.Color.green())
+    try:
+        with open("sources.json","r") as k:
+            srcs = json.load(k)
+    except:
+        await ctx.send("ERROR")
+        return await ctx.author.send("ERROR: 'Not configured `sources.json` yet. Please run [p]csetup'")
+    amt = 0
+    for item in srcs:
+        if amt == 5:
+            pages.append(embed)
+            paged = True
+            embed = discord.Embed(title="Repos",description=f"Sources for {bot.name}",color=discord.Color.green())
+            amt = 0
+        else:
+            link = srcs[item]
+            release = f"{link}/release.json"
+            release = release.replace("//","/")
+            release = release.replace("https:/","https://")
+            packages = f"{link}/packages.json"
+            packages = packages.replace("//","/")
+            packages = packages.replace("https:/","https://")
+            rel = requests.request("GET",url=release)
+            rel = rel.json()
+            pkgs = requests.request("GET",url=packages)
+            pkgs = pkgs.json()
+            """
+            icon = f"{link}/logo.ico"
+            icon = icon.replace("//","/")
+            icon = icon.replace("https:/","https://")
+            try:
+                embed.set_image(url=icon)
+            except:
+                embed.set_image(url="https://gw.alipayobjects.com/zos/antfincdn/4zAaozCvUH/unexpand.svg")
+            """ # try for PIL with sileo img
+            pkgtotal = pkgs["packagetotal"]
+            author = rel["author"]
+            description = rel["description"]
+            embed.add_field(name=item,value=f"[Click Me]({link}) | `{pkgtotal}` Package(s) | By *{author}*\n**description:** `{description}`",inline=False)
+            amt += 1
+    if paged == False:
+        pages.append(embed)
+    await paginate(bot,ctx,pages)
+
+
+async def instmod(ctx,alias,link):
     try:
         cogd = "cogs/"
         loadring = await ctx.send(embed=discord.Embed(title="CogMaster",description="getting module...",color=ctx.author.color))
-        first = False
-        try:
-            os.system("cd tempcfg")
-        except FolderNotFound:
-            os.system("mkdir tempcfg")
-        try:
-            os.system("rm -rf tempcfg/cogcfg.json")
-        except:
-            first = True
-        os.system(f"cd tempcfg && wget {cfg}")
-        with open("tempcfg/cogcfg.json","r") as f:
-            mod = json.load(f)
+        release = f"{link}/release.json"
+        release = release.replace("//","")
+        packages = f"{link}/packages.json"
+        packages = packages.replace("//","")
+        rel = requests.request("GET",release)
+        rel = rel.json()
+        pkgs = requests.request("GET",packages)
+        pkgs = pkgs.json()
         with open("config.json","r") as k:
             cefg = json.load(k)
         try:
@@ -50,10 +132,8 @@ async def instmod(ctx,cfg):
             author = mod["author"]
             files = mod["files"]
             modules = mod["modules"]
-            execx = mod["exec"]
-            pyexec = mod["pyexec"]
         except KeyError:
-            return await ctx.author.send("ERROR: 'Cogcfg.json not made properly'")
+            return await ctx.author.send("ERROR: 'release.json not made properly'")
         try:
             cogd = cefg["directory"]
         except KeyError:
@@ -66,7 +146,7 @@ async def instmod(ctx,cfg):
         for url in files:
             os.system(f"cd {cogd}/{name} && wget {url}")
         for url2 in modules:
-            os.system(f"cd modules/{name} && wget {url2}")    
+            os.system(f"cd modules/{name} && wget {url2}")
         await loadring.edit(embed=discord.Embed(title="CogMaster",description="downloaded files...",color=ctx.author.color))
         for file in listdir(f'{cogd}/{name}'):
             if file.endswith('.py'):
@@ -78,15 +158,15 @@ async def instmod(ctx,cfg):
             cefg["toload"] = []
             cefg["toload"].append(name)
         with open("config.json","w") as x:
-            json.dump(x)
+            json.dump(cefg,x)
         await loadring.edit(embed=discord.Embed(title="CogMaster",description="reloading cogs...",color=ctx.author.color))
         with open("config.json","r") as z:
             cefgx = json.load(z)
         for ext in cefgx["toload"]:
             for file in listdir(f'{cogd}/{ext}'):
-            if file.endswith('.py'):
-                bot.unload_extension(f'{cogd}.{ext}.{file[:-3]}')
-                bot.load_extension(f'{cogd}.{ext}.{file[:-3]}')
+                if file.endswith('.py'):
+                    bot.unload_extension(f'{cogd}.{ext}.{file[:-3]}')
+                    bot.load_extension(f'{cogd}.{ext}.{file[:-3]}')
         await loadring.edit(embed=discord.Embed(title="CogMaster",description="Running postinstall, kindly check terminal",color=ctx.author.color))
         if execx != "":
             query = input(f"Would you like to run '{execx}' in terminal (y/n)?")
@@ -108,13 +188,15 @@ async def instmod(ctx,cfg):
                 print("aborted")
         await loadring.edit(embed=discord.Embed(title="CogMaster",description="Cleaning up!",color=ctx.author.color))
         for filez in listdir("tempcfg"):
-            if filez.endswith(".json") or if filez.endswith(".json"):
-                os.system(f"rm -rf tempcfg/{file[:-3]}")
+            if filez.endswith(".json"):
+                os.system(f"rm -rf tempcfg/{filez[:-3]}")
+            if filez.endswith(".py"):
+                os.system(f"rm -rf tempcfg/{filez[:-3]}")
         await asyncio.sleep(1)
         await loadring.edit(embed=discord.Embed(title="CogMaster",description=f"Finished installing {name} v{version} by {author}",color=discord.Color.green()))
     except:
         await loadring.edit(embed=discord.Embed(title="CogMaster",description=f"Failed install",color=discord.Color.red()))
-        
+
 
 def log_write(text):
     with open("log.log","a") as log:
@@ -124,31 +206,46 @@ def log_write(text):
 
 log_write("starting cogmaster")
 
-bot = commands.AutoShardedBot(command_prefix='cm!')
-bot.remove_command('help')
-
 with open("config.json","r") as z:
     cefgx = json.load(z)
+try:
+    prefix = cefgx["prefix"]
+except KeyError:
+    prefix = input("Enter prefix: ")
+cefgx["prefix"] = prefix
+with open("config.json","w") as zx:
+    json.dump(cefgx,zx)
+bot = commands.AutoShardedBot(command_prefix=prefix)
+bot.remove_command('help')
+
+
 for ext in cefgx["toload"]:
-    for file in listdir(f'{cefg["directory"]}/{ext}'):
+    for file in listdir(f'{cefgx["directory"]}/{ext}'):
         if file.endswith('.py'):
-            bot.load_extension(f'{cefg["directory"]}.{ext}.{file[:-3]}')
-for file in listdir(f'MyOwn/'):
-    if file.endswith('.py'):
-        bot.load_extension(f'MyOwn.{file[:-3]}')
+            #bot.unload_extension(f'{cefgx["directory"]}.{ext}.{file[:-3]}')
+            bot.load_extension(f'{cefgx["directory"]}.{ext}.{file[:-3]}')
+try:
+    for file in listdir(f'MyOwn/'):
+        if file.endswith('.py'):
+            bot.load_extension(f'MyOwn.{file[:-3]}')
+except:
+    ok = "Fail"
 
 @bot.event
 async def on_ready():
     DiscordComponents(bot)
     await bot.change_presence(activity=discord.Watching(name='Cogs in the wall'))
     log_write('We have logged in as {0.user}'.format(bot))
-    
-@bot.command(aliases=["cinstall","ci","cinst"])
-async def coginstall(ctx,repo):
+
+@bot.command(aliases=["source"])
+async def repo(ctx,action,alias,repo):
     if ctx.author.id not in devs:
         return await ctx.send("ERROR:'Developer requirement'")
-    linktocfg = getmodule(repo)
-    await instmod(ctx,linktocfg)
+    if action == "+" or action == "add":
+        await addrepo(ctx,alias,repo)
+@bot.command(aliases=["sources"])
+async def repos(ctx):
+    await sourcelist(ctx)
 
 @bot.command(aliases=["cremove","cr","crm"])
 async def cogremove(ctx,module):
@@ -175,14 +272,21 @@ async def cogsetup(ctx,directory=None):
     comment = ""
     with open("config.json","r") as l:
         cefg = json.load(l)
-    if cefg["setup"] == "done":
-        comment = "re-"
+    try:
+        if cefg["setup"] == "done":
+            comment = "re-"
+    except:
+        ok = "fail"
     cefg["directory"] = directory
     os.system(f"mkdir {directory}")
     os.system(f"mkdir MyOwn")
+    os.system(f"mkdir lib")
+    os.system("touch sources.json")
+    with open("sources.json","w") as sources:
+        sources.write("{}")
     cefg["setup"] = "done"
     with open("config.json","w") as l:
         json.dump(cefg,l)
     await ctx.send(f"{comment}Setup successfull")
-    
+
 bot.run("token")
