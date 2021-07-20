c5f02d3d6189120548891653a8e3daf4713cb660
Kaneki <cool5tarXV@gmail.com>
2021-07-20 18:57:34 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,32 @@
+release-example-json = {
+"name": "<Your Repo’s Name>",
+"category": [],
+"suite": "stable",
+"Version": 1,
+"author": "cydia-ios",
+"description": "description",
+"install_msg": "thank you for installing this repo",
+"agreement": "To install this we will be collecting .... and this can impact your privacy"
+}
+
+info-example-json = {
+    "author": ["PhenoM4n4n"],
+    "install_msg": "Thanks for installing AltDentifier!\nGet started with `[p]help AltDentifier`.",
+    "files":[],
+    "lib":[],
+    "name": "AltDentifier",
+    "disabled": false,
+    "short": "Check users with AltDentifier API",
+    "description": "Check users with AltDentifier API",
+    "tags": [],
+    "requirements": ["unidecode", "rapidfuzz"],
+    "hidden": false,
+    "end_user_data_statement": "This cog does not store any End User Data."
+}
+
+packages-example-json = {
+"reponame": "example",
+"packagetotal": 1
+"packagename": "/pathtopackage/",
+"example": "/example/"
+}
